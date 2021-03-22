49f0ee1989ef30fac43534b24ed446fab60a6087
aethernet <deutherity@gmail.com>
2021-03-22 21:01:31 +0300

    added comments

--- findconfdir.hpp
+++ findconfdir.hpp
@@ -1,3 +1,8 @@
 #include <string>
 
 std::string findconfdir(bool create = true);
+// Returns string with config directory
+// throws 1 if something besides a directory
+// throws 1 if create = false and path doesnt exist
+// creates dir if create = true
+// also checks XDG_CONFIG_HOME env variable
