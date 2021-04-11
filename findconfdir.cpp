fccbf258649aa1f7e70faaffe4fb2dc2931a6357
aethernet <deutherity@gmail.com>
2021-04-11 23:48:30 +0300

    formatted via clang-format. amazing

--- findconfdir.cpp
+++ findconfdir.cpp
@@ -1,37 +1,31 @@
 #include "findconfdir.hpp"
-#include <filesystem>
-#include <cstdlib>
 #include "projectname.hpp"
+#include <cstdlib>
+#include <filesystem>
 extern "C" {
-#include <unistd.h>
-#include <sys/types.h>
 #include <pwd.h>
+#include <sys/types.h>
+#include <unistd.h>
 }
 
-
 namespace fs = std::filesystem;
-std::string findconfdir(bool create)
-{
+std::string findconfdir(bool create) {
     fs::path respath;
     const char *config_path = std::getenv("XDG_CONFIG_HOME");
     if (config_path != nullptr)
         respath = config_path;
-    else
-    {
+    else {
         respath = getpwuid(getuid())->pw_dir;
         respath /= ".config";
     }
     respath /= PROJECTNAME;
-    if (create)
-    {
+    if (create) {
         if (!fs::exists(respath))
             fs::create_directory(respath);
         else if (!fs::is_directory(respath))
             throw 1;
         return respath.string();
-    }    
-    else if (!fs::exists(respath) || !fs::is_directory(respath))
+    } else if (!fs::exists(respath) || !fs::is_directory(respath))
         throw 1;
     return respath.string();
-
 }
